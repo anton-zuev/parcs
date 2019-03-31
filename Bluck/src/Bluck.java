86d917c37057fd896f0b51119955c81049bfdca8
Anton Zuev <ant0n3uev@gmail.com>
2019-03-31 12:52:53 +0300

    Fixes

--- Bluck/src/Bluck.java
+++ Bluck/src/Bluck.java
@@ -22,21 +22,4 @@ public class Bluck {
         curtask.end();
     }
 
-    public static Node fromFile(String filename) throws Exception {
-        Scanner sc = new Scanner(new File(filename));
-        int m = sc.nextInt();
-        int s = sc.nextInt();
-        List<Node> nodes = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            nodes.add(new Node(i + 1));
-        }
-        for (Node n : nodes) {
-            n.setTime(sc.nextInt());
-            int k = sc.nextInt();
-            for (int j = 0; j < k; j++) {
-                n.addDep(nodes.get(sc.nextInt() - 1));
-            }
-        }
-        return nodes.get(s - 1);
-    }
 }
